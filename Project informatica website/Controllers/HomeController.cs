bed96e4880e77b027b762ee4d25731dcae08cd65
Mark Kellerhuis <mark@kellerhuis.nl>
2021-05-12 19:19:18 +0200

    MySQL toegevoegd en getest met namen uit product DBase

--- Project informatica website/Controllers/HomeController.cs
+++ Project informatica website/Controllers/HomeController.cs
@@ -4,8 +4,10 @@ using Project_informatica_website.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using MySql.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Project_informatica_website.Controllers
 {
@@ -20,9 +22,47 @@ namespace Project_informatica_website.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+            // alle namen ophalen
+            var names = GetNames();
 
+            // stop de namen in de HTML
+            return View(names);
+        }
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
+        }
         public IActionResult Privacy()
         {
             return View();
