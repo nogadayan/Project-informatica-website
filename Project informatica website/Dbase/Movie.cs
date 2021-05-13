2cec38f167ecb2bb479847ec81a0e7d62195e170
Mark Kellerhuis <mark@kellerhuis.nl>
2021-05-13 12:28:10 +0200

    Movie structure toevoegevoed

---
+++ Project informatica website/Dbase/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_informatica_website.Dbase
+{
+    public class Movie
+    {
+        public string IMDB { get; set; }
+
+        public string Title { get; set; }
+
+        public string Release_Date { get; set; }
+
+        public string Plot { get; set; }
+
+     
+    }
+}
