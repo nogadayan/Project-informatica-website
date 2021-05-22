2bb15c0b34799d74acadb011f0bd9c96e6b429db
Mark Kellerhuis <mark@kellerhuis.nl>
2021-05-22 13:39:46 +0200

    Werken aan Movie API koppeling: Class MoviefromAPI toegevoegd movie bestond al want is een van de Tabellen

---
+++ Project informatica website/Dbase/MoviefromAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_informatica_website.Dbase
+{
+    public class MoviefromAPI
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Plot { get; set; }
+        public string Actors { get; set; }
+        public string ImdbRating { get; set; }
+    }
+}
