2ce5c2333b68e54ef11eac79444febac273cdff8
noga dayan <110382@st-maartenscollege,nl>
2021-06-04 12:30:27 +0200

    server wijzigen naar 172.16.160.21 pt.2

--- Project informatica website/Controllers/HomeController.cs
+++ Project informatica website/Controllers/HomeController.cs
@@ -145,7 +145,7 @@ namespace Project_informatica_website.Controllers
         public List<Movie> GetMovies(string imdb_tt)
         {
             // stel in waar de database gevonden kan worden
-            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
 
             // maak een lege lijst waar we de namen in gaan opslaan
             List<Movie> Movie = new List<Movie>();
