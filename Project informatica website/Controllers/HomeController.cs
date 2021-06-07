6a00ad11dc18042046b18f132682cb1e67d73eaa
noga dayan <110382@st-maartenscollege,nl>
2021-06-07 21:13:19 +0200

    server weer veranderd naar 172.16.160.21

--- Project informatica website/Controllers/HomeController.cs
+++ Project informatica website/Controllers/HomeController.cs
@@ -138,7 +138,7 @@ namespace Project_informatica_website.Controllers
         public List<string> GetNames()
         {
             // stel in waar de database gevonden kan worden
-            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
 
             // maak een lege lijst waar we de namen in gaan opslaan
             List<string> names = new List<string>();
