8607f9c00c7ccddce601dab516fe4c49bd6b48b8
Mark Kellerhuis <mark@kellerhuis.nl>
2021-06-10 23:12:02 +0200

    GetUsers() in HomeController

--- Project informatica website/Controllers/HomeController.cs
+++ Project informatica website/Controllers/HomeController.cs
@@ -378,6 +378,44 @@ namespace Project_informatica_website.Controllers
             // return de lijst met movies
             return Movie_actors;
         }
+        public List<Users> GetUsers()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110382;Uid=110382;Pwd=inf2021sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Users> Users = new List<Users>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from users", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        Users m = new Users
+                       {
+                        Username = reader["Username"].ToString(),
+                        Password = reader["Password"].ToString(),
+                        };
+                        // voeg de naam toe aan de lijst met namen
+                        Users.Add(m);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return Users;
+        }
         [Route("Action_page")]
         public IActionResult Action_page(string firstname, string lastname, string country, string message)
         {
