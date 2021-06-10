d3cd1b066d773c4c2c4678267bbe6fb9fb8221ff
Mark Kellerhuis <mark@kellerhuis.nl>
2021-06-10 23:01:05 +0200

    Public class voor de Users aangemaakt

---
+++ Project informatica website/Dbase/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_informatica_website.Dbase
+{
+    public class Users
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
