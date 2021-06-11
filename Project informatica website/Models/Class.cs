d019e92e5bf18e95699e13e62a5462643f492762
Mark Kellerhuis <mark@kellerhuis.nl>
2021-06-11 21:28:53 +0200

    Error Messages opgenomen in Class

--- Project informatica website/Models/Class.cs
+++ Project informatica website/Models/Class.cs
@@ -3,13 +3,24 @@
 namespace SendAndStore.Models
 {
     public class Person 
-    { 
-        [Required]
+    {
+        [Required(ErrorMessage = "Please enter your First Name")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Please enter your Last Name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Email address is needed")]
+        [EmailAddress(ErrorMessage = "Not valid email address")]
         public string Email { get; set; }
+
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Message needs to be filled out")]
+        [Display(Name = "Message")]
         public string Description { get; set; }
 
     }
