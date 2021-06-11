6c378e7b5f281784ec9412b8eb9cd618eec83c9c
noga dayan <110382@st-maartenscollege,nl>
2021-06-11 14:52:06 +0200

    nieuwe bestand gemaakt en ingevuld voor "person"

---
+++ Project informatica website/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace SendAndStore.Models
+{
+    public class Person 
+    { 
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string country { get; set; }
+        public string message { get; set; }
+
+    }
+}
